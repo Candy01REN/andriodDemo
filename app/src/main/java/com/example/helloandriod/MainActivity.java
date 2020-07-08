8a2e4d755aef48ea723b2a4cf8339ecb4478947f
Candy01REN <1970524081@qq.com>
2020-07-09 07:39:39 +0800

    第一个安卓项目练习

---
+++ app/src/main/java/com/example/helloandriod/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.helloandriod;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //以下五行自己实现的数字加倍
+                TextView textValue=findViewById(R.id.text_value);//获取对应id的值
+                String textString=textValue.getText().toString();//获取数字并转换成字符串的形式
+                int originalValue=Integer.parseInt(textString);//把string值转换成interger形式
+                int newValue=MyWorker.doubleTheValue(originalValue);//对数字翻倍
+                textValue.setText(Integer.toString(newValue));//把新的值赋值到屏幕上
+
+                Snackbar.make(view, "从"+originalValue+"变成"+newValue, Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
